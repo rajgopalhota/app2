e4a5589dc62cdd6ddf0cb823696ca4f3ecd47200
RAJGOPAL HOTA <2100032351@KLUNIVERSITY.IN>
2023-01-09 16:11:05 +0530

    back navigation

--- src/components/Ordersummary.js
+++ src/components/Ordersummary.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Ordersummary() {
+  const navigate = useNavigate();
   return (
     <div>
-        <h1>Order is Confirmed!!!</h1>
+      <h1>Order is Confirmed!!!</h1>
+      <button onClick={() => navigate(-1)}>Go back</button>
     </div>
   )
 }
